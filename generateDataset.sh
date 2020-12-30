07adceb47ed7a036b3ca415a14a6679b374a2391
Gissella <gissemari@gmail.com>
2020-12-30 11:45:53 -0500

    generate data

---
+++ generateDataset.sh
@@ -0,0 +1,4 @@
+
+python segmentPerSRT.py --rawVideoPath /home/gissella/Documents/Research/SignLanguage/PeruvianSignLanguaje/Data/Videos/RawVideo/ --srtPath /home/gissella/Documents/Research/SignLanguage/PeruvianSignLanguaje/Data/SRT/SRT_gestures/ --outputVideoPath /home/gissella/Documents/Research/SignLanguage/PeruvianSignLanguaje/Data/Videos/Segmented_gestures/ --flgGesture 1
+
+python ConvertVideoToKeypoint.py --holistic
