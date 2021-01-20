7252d33c59be093f40cc1da68959e58a793db2e7
JoeNatan30 <huamani.jn@pucp.edu.pe>
2021-01-19 23:48:43 -0500

    sh - press q to exit option

--- generateDataset.sh
+++ generateDataset.sh
@@ -1,4 +1,18 @@
 
-python segmentPerSRT.py --rawVideoPath /home/gissella/Documents/Research/SignLanguage/PeruvianSignLanguaje/Data/Videos/RawVideo/ --srtPath /home/gissella/Documents/Research/SignLanguage/PeruvianSignLanguaje/Data/SRT/SRT_gestures/ --outputVideoPath /home/gissella/Documents/Research/SignLanguage/PeruvianSignLanguaje/Data/Videos/Segmented_gestures/ --flgGesture 1
+python 2.Segmentation/segmentPerSRT.py --rawVideoPath /home/gissella/Documents/Research/SignLanguage/PeruvianSignLanguaje/Data/Videos/RawVideo/ --srtPath /home/gissella/Documents/Research/SignLanguage/PeruvianSignLanguaje/Data/SRT/SRT_gestures/ --outputVideoPath /home/gissella/Documents/Research/SignLanguage/PeruvianSignLanguaje/Data/Videos/Segmented_gestures/ --flgGesture 1
 
-python ConvertVideoToKeypoint.py --holistic
+python 3.Translation/FrameToKeypoint/ConvertVideoToKeypoint.py --holistic
+
+echo "Press 'q' to exit"
+count=0
+while : ; do
+read -n 1 k <&1
+if [[ $k = q ]] ; then
+printf "\nQuitting from the program\n"
+break
+else
+((count=$count+1))
+printf "\nIterate for $count times\n"
+echo "Press 'q' to exit"
+fi
+done
