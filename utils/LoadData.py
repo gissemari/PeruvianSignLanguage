d132bb074548f8a620b12760ed4784e84cc702d7
JoeNatan30 <huamani.jn@pucp.edu.pe>
2021-01-25 00:10:02 -0500

    load Data - utils

---
+++ utils/LoadData.py
@@ -0,0 +1,126 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 22 23:40:24 2021
+
+@author: Joe
+"""
+# Standard library imports
+import os
+from collections import Counter
+
+# Third party imports
+import pandas as pd
+
+# Local imports
+
+
+def ReduceDataToMinimunSize(x, y):
+
+    # to count repeated targets in y
+    targetDict = dict(Counter(y))
+
+    # get the least mount of data of all categories
+    minimun = min(targetDict.values())
+
+    newX = []
+    newY = []
+
+    # to set a counter for all categories to the minimun data
+    reverseCounter = dict.fromkeys(targetDict, minimun)
+
+    for index, value in enumerate(y):
+
+        # if reverseCounter is not zero(value)
+        if reverseCounter[value]:
+
+            newX.append(x[index])
+            newY.append(y[index])
+
+            # to do reverse counter from minimun to zero
+            reverseCounter[value] = reverseCounter[value] - 1
+
+    return newX, newY
+
+
+# getlist of 2D tuple of the most common N words of a filename in a group
+# of folders
+def getTopNWords(nWords, mainFolderPath):
+
+    foldersToLoad = os.listdir(mainFolderPath)
+
+    wordList = []
+
+    for folder in foldersToLoad:
+
+        # if the file have extension then that file will be omited
+        if(os.path.splitext(folder)[1] != ''):
+            continue
+
+        folder = os.listdir(mainFolderPath+folder)
+
+        # wordList => list that acumulate the name of all files of all folders.
+        # The acumulated names will have some modification made by
+        # file.split('_')[0]
+        #
+        # file.split('_')[0] => get the name of the file without "_" and
+        # its number. then, these names are acumulated in a list that will
+        # be added in wordList
+
+        wordList = wordList + [file.split('_')[0] for file in folder]
+
+    # Convert the given list into dictionary
+    # the output will be like {'ENTONCES':2,'HOY':3,'MAL':2, ...}
+    wordTrends = Counter(wordList)
+
+    # most_common function create a 2D tuple of the N most common words in
+    # the dict
+    return wordTrends.most_common(nWords)
+
+
+def getTopNWordData(nWords, mainFolderPath, minimun=False):
+
+    # return a list of 2D tuple
+    top20Words = getTopNWords(nWords, mainFolderPath)
+
+    topWordList = [key for (key, value) in top20Words]
+    wordLabels = list(range(len(topWordList)))
+
+    # To identify which word corresponds to its label (int: 1, 2, 3, ...)
+    #
+    # topWordList will be the key list
+    # wordLabels will be the values list
+    topWordDict = dict(zip(topWordList, wordLabels))
+
+    x = []
+    y = []
+
+    foldersToLoad = os.listdir(mainFolderPath)
+
+    for folderName in foldersToLoad:
+
+        # if the file have extension then that file will be omited
+        if(os.path.splitext(folderName)[1] != ''):
+            continue
+
+        folderList = os.listdir(mainFolderPath+folderName)
+
+        for file in folderList:
+
+            word = file.split('_')[0]
+
+            if word not in topWordList:
+                continue
+
+            fileData = pd.read_pickle(mainFolderPath+folderName+'/'+file)
+
+            for data in fileData:
+                x.append(data)
+                y.append(topWordDict[word])
+
+    # To get the category with the least amount of data. So, all categories
+    # have the same amount of data
+    if(minimun):
+        return ReduceDataToMinimunSize(x, y)
+
+    else:
+        return x, y
