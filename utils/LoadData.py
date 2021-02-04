bf766e1a5c3913d8987f02033e286ce007bbc75b
JoeNatan30 <huamani.jn@pucp.edu.pe>
2021-02-04 16:44:07 -0500

    LoadData.py output corecctions

--- utils/LoadData.py
+++ utils/LoadData.py
@@ -10,6 +10,8 @@ from collections import Counter
 
 # Third party imports
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
 # Local imports
 
@@ -18,9 +20,11 @@ def splitData(x, y, split=0.8):
 
     # to count repeated targets in y
     targetDict = dict(Counter(y))
+
     pivot = targetDict.copy()
 
     for key in targetDict:
+
         pivot[key] = int(targetDict[key]*split)
 
     x_train = []
@@ -78,14 +82,15 @@ def getTopNWords(nWords, mainFolderPath):
     foldersToLoad = os.listdir(mainFolderPath)
 
     wordList = []
+    timeStepDict = {}
 
-    for folder in foldersToLoad:
+    for folderName in foldersToLoad:
 
         # if the file have extension then that file will be omited
-        if(os.path.splitext(folder)[1] != ''):
+        if(os.path.splitext(folderName)[1] != ''):
             continue
 
-        folder = os.listdir(mainFolderPath+folder)
+        folder = os.listdir(mainFolderPath+folderName)
 
         # wordList => list that acumulate the name of all files of all folders.
         # The acumulated names will have some modification made by
@@ -94,22 +99,59 @@ def getTopNWords(nWords, mainFolderPath):
         # file.split('_')[0] => get the name of the file without "_" and
         # its number. then, these names are acumulated in a list that will
         # be added in wordList
+        #
+        # timeStepDict => dictionary that acumulate the number of timestep  of
+        # each file in its respective word
 
-        wordList = wordList + [file.split('_')[0] for file in folder]
+        for file in folder:
+
+            word = file.split('_')[0]
+
+            fileData = pd.read_pickle(mainFolderPath+folderName+'/'+file)
+
+            if word in timeStepDict:
+                timeStepDict[word] = timeStepDict[word] + [len(fileData)]
+                wordList = wordList + [word]
+            else:
+                timeStepDict.update({word: [len(fileData)]})
 
     # Convert the given list into dictionary
     # the output will be like {'ENTONCES':2,'HOY':3,'MAL':2, ...}
     wordTrends = Counter(wordList)
 
-    # most_common function create a 2D tuple of the N most common words in
-    # the dict
-    return wordTrends.most_common(nWords)
+    # most_common function create a 2D tuple of the N most common words of
+    # the Counter
+    topWords = wordTrends.most_common(nWords)
+
+    # to acumulate timesteps to do statistic
+    union = []
+
+    for word, value in topWords:
+        union = union + timeStepDict[word]
+
+    plt.hist(union, bins=max(union))
+    plt.xlabel("Number of time steps")
+    plt.ylabel("frequency")
+    plt.show
+
+    unionArr = np.asarray(union)
+    mean = unionArr.mean()
+    std = unionArr.std()
+
+    '''
+    print("Number of instances:")
+    print("-------------------")
+    print("2nd deviation range: ", [mean - 2 * std, mean + 2 * std])
+    print("Minimun: %d" % min(union))
+    print("Maximun: %d" % max(union))
+    '''
+    return topWords, int(mean + 2 * std)+1
 
 
 def getTopNWordData(nWords, mainFolderPath, minimun=False):
 
     # return a list of 2D tuple
-    top20Words = getTopNWords(nWords, mainFolderPath)
+    top20Words, maxTimeStepAllowed = getTopNWords(nWords, mainFolderPath)
 
     topWordList = [key for (key, value) in top20Words]
     wordLabels = list(range(len(topWordList)))
@@ -125,6 +167,7 @@ def getTopNWordData(nWords, mainFolderPath, minimun=False):
 
     foldersToLoad = os.listdir(mainFolderPath)
 
+    # folders
     for folderName in foldersToLoad:
 
         # if the file have extension then that file will be omited
@@ -133,6 +176,7 @@ def getTopNWordData(nWords, mainFolderPath, minimun=False):
 
         folderList = os.listdir(mainFolderPath+folderName)
 
+        # instances
         for file in folderList:
 
             word = file.split('_')[0]
@@ -142,14 +186,17 @@ def getTopNWordData(nWords, mainFolderPath, minimun=False):
 
             fileData = pd.read_pickle(mainFolderPath+folderName+'/'+file)
 
-            for data in fileData:
-                x.append(data)
-                y.append(topWordDict[word])
+            timeStepSize = len(fileData)
 
-    # To get the category with the least amount of data. So, all categories
-    # have the same amount of data
+            if timeStepSize > maxTimeStepAllowed:
 
-    if(minimun):
-        x, y = ReduceDataToMinimunSize(x, y)
+                continue
+            fileData = fileData.flatten()
+            for _ in range(maxTimeStepAllowed - timeStepSize):
 
-    return x, y
+                fileData = np.append(fileData, [np.zeros(66)])
+
+            x = x + [list(fileData)]
+            y = y + [topWordDict[word]]
+
+    return x, y, maxTimeStepAllowed
