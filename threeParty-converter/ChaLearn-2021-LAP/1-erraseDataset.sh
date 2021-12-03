8ad04c78fee5f392f8591a1116ec23c55dbdcabc
JoeNatan30 <huamani.jn@pucp.edu.pe>
2021-12-03 01:26:15 -0500

    yaml and sh

---
+++ threeParty-converter/ChaLearn-2021-LAP/1-erraseDataset.sh
@@ -0,0 +1,15 @@
+rm data/*csv
+rm project/*csv
+echo "csv removed"
+rm -r project/data/kp/train/*
+rm -r project/data/kp/val/*
+rm -r project/data/kp/test/*
+echo "keypoints removed"
+rm -r project/data/mp4/test/*
+rm -r project/data/mp4/val/*
+rm -r project/data/mp4/train/*
+echo "videos removed"
+rm -r project/data/kpflow2/train/*
+rm -r project/data/kpflow2/test/*
+rm -r project/data/kpflow2/val/*
+echo "keypoint flow removed"
