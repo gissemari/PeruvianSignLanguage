f566ada1db0039eb9954425ef23fac7257f008b3
Chameleon Cloud User <cc@instlrec.novalocal>
2021-12-26 06:53:52 +0000

    chameleon sh files

--- 3.Classification/ChaLearn-2021-LAP/2-generateDataset-AEC-chame.sh
+++ 3.Classification/ChaLearn-2021-LAP/2-generateDataset-AEC-chame.sh
@@ -5,11 +5,11 @@ echo "generating data in Peruvian Sign language project..."
 sleep 1
 cd ../..
 cp -r utils 5.Preparation
-python 5.Preparation/DictToSample.py --dict_Path ../DataReal/AEC/Dataset/dict/dict.json --shuffle --output_Path ../DataReal/AEC/Dataset/readyToRun/ --words_File ../DataReal/list.csv --words 10
+python 5.Preparation/DictToSample.py --dict_Path Data/AEC/Dataset/dict/dict.json --shuffle --output_Path Data/AEC/Dataset/readyToRun/ --words_File Data/list.csv --words 10
 echo ""
 echo "generating csv in Chalearn..."
 cd 3.Classification/ChaLearn-2021-LAP/data
-python excel.py --dictPath ../../../../DataReal/AEC/Dataset/dict/dict.json --keyPath ../../../../DataReal/AEC/Dataset/readyToRun/
+python excel.py --dictPath ../../../Data/AEC/Dataset/dict/dict.json --keyPath ../../../Data/AEC/Dataset/readyToRun/
 python stage2.py 1
 cd ..
 cp data/train_val_labels_STAGE2.csv project
@@ -17,11 +17,11 @@ echo ""
 echo "train_val_labels_STAGE2.csv copied in project file"
 echo ""
 echo "obtaining videos from Peruvian Sign language project..."
-python obtain_videos.py --allfiles ../../../DataReal/AEC/Videos/cropped/
+python obtain_videos.py --allfiles ../../Data/AEC/Videos/cropped/
 echo "counting frames..."
 python count_frames.py
 echo "extracting keypoints..."
-python extract_keypoint.py --src ../../../DataReal/AEC/Dataset/keypoints/ --keyPath ../../../DataReal/AEC/Dataset/readyToRun/
+python extract_keypoint.py --src ../../Data/AEC/Dataset/keypoints/ --keyPath ../../Data/AEC/Dataset/readyToRun/
 echo "extrancting poseflow..."
 python extract_poseflow.py
 
