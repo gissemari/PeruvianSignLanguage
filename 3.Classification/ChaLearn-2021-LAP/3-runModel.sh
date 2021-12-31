15f9a8e9113ea57e942a29b91aa76530a4da767b
JoeNatan30 <huamani.jn@pucp.edu.pe>
2021-12-31 13:12:23 -0500

    Oso changes to predict

--- 3.Classification/ChaLearn-2021-LAP/3-runModel.sh
+++ 3.Classification/ChaLearn-2021-LAP/3-runModel.sh
@@ -1,2 +1,32 @@
- python train.py --log_dir project/log --dataset handcrop_poseflow --num_workers 4 --sequence_length 16 --temporal_stride 2 --learning_rate 1e-4 --gradient_clip_val=1 --gpus 0 --cnn rn34 --num_layers 4 --num_heads 8 --batch_size 4 --accumulate_grad_batches 8 --data_dir project/data/mp4 --model VTN_HCPF
+python train.py --log_dir project/log --dataset handcrop_poseflow --num_workers 4 --sequence_length 20 --temporal_stride 1 --learning_rate 1e-4 --gradient_clip_val=1 --gpus 1 --cnn rn34 --num_layers 4 --num_heads 8 --batch_size 4 --accumulate_grad_batches 8 --data_dir project/data/mp4 --model VTN_HCPF  --num_classes 10
+
+python train.py --log_dir project/log --dataset handcrop_poseflow --num_workers 4 --sequence_length 20 --temporal_stride 1 --learning_rate 1e-3 --gradient_clip_val=1 --gpus 1 --cnn rn34 --num_layers 4 --num_heads 8 --batch_size 4 --accumulate_grad_batches 8 --data_dir project/data/mp4 --model VTN_HCPF  --num_classes 10
+
+python train.py --log_dir project/log --dataset handcrop_poseflow --num_workers 4 --sequence_length 20 --temporal_stride 2 --learning_rate 1e-4 --gradient_clip_val=1 --gpus 1 --cnn rn34 --num_layers 4 --num_heads 8 --batch_size 4 --accumulate_grad_batches 8 --data_dir project/data/mp4 --model VTN_HCPF  --num_classes 10
+
+python train.py --log_dir project/log --dataset handcrop_poseflow --num_workers 4 --sequence_length 20 --temporal_stride 2 --learning_rate 1e-3 --gradient_clip_val=1 --gpus 1 --cnn rn34 --num_layers 4 --num_heads 8 --batch_size 4 --accumulate_grad_batches 8 --data_dir project/data/mp4 --model VTN_HCPF  --num_classes 10
+
+python train.py --log_dir project/log --dataset handcrop_poseflow --num_workers 4 --sequence_length 20 --temporal_stride 1 --learning_rate 1e-4 --gradient_clip_val=1 --gpus 1 --cnn rn34 --num_layers 4 --dropout 0.25 --num_heads 8 --batch_size 4 --accumulate_grad_batches 8 --data_dir project/data/mp4 --model VTN_HCPF  --num_classes 10
+
+python train.py --log_dir project/log --dataset handcrop_poseflow --num_workers 4 --sequence_length 20 --temporal_stride 1 --learning_rate 1e-3 --gradient_clip_val=1 --gpus 1 --cnn rn34 --num_layers 4 --dropout 0.25 --num_heads 8 --batch_size 4 --accumulate_grad_batches 8 --data_dir project/data/mp4 --model VTN_HCPF  --num_classes 10
+
+python train.py --log_dir project/log --dataset handcrop_poseflow --num_workers 4 --sequence_length 20 --temporal_stride 2 --learning_rate 1e-4 --gradient_clip_val=1 --gpus 1 --cnn rn34 --num_layers 4 --dropout 0.25 --num_heads 8 --batch_size 4 --accumulate_grad_batches 8 --data_dir project/data/mp4 --model VTN_HCPF  --num_classes 10
+
+python train.py --log_dir project/log --dataset handcrop_poseflow --num_workers 4 --sequence_length 20 --temporal_stride 2 --learning_rate 1e-3 --gradient_clip_val=1 --gpus 1 --cnn rn34 --num_layers 4 --dropout 0.25 --num_heads 8 --batch_size 4 --accumulate_grad_batches 8 --data_dir project/data/mp4 --model VTN_HCPF  --num_classes 10
+
+python train.py --log_dir project/log --dataset handcrop_poseflow --num_workers 4 --sequence_length 25 --temporal_stride 1 --learning_rate 1e-4 --gradient_clip_val=1 --gpus 1 --cnn rn34 --num_layers 4 --num_heads 8 --batch_size 4 --accumulate_grad_batches 8 --data_dir project/data/mp4 --model VTN_HCPF  --num_classes 10
+
+python train.py --log_dir project/log --dataset handcrop_poseflow --num_workers 4 --sequence_length 25 --temporal_stride 1 --learning_rate 1e-3 --gradient_clip_val=1 --gpus 1 --cnn rn34 --num_layers 4 --num_heads 8 --batch_size 4 --accumulate_grad_batches 8 --data_dir project/data/mp4 --model VTN_HCPF  --num_classes 10
+
+python train.py --log_dir project/log --dataset handcrop_poseflow --num_workers 4 --sequence_length 25 --temporal_stride 2 --learning_rate 1e-4 --gradient_clip_val=1 --gpus 1 --cnn rn34 --num_layers 4 --num_heads 8 --batch_size 4 --accumulate_grad_batches 8 --data_dir project/data/mp4 --model VTN_HCPF  --num_classes 10
+
+python train.py --log_dir project/log --dataset handcrop_poseflow --num_workers 4 --sequence_length 25 --temporal_stride 2 --learning_rate 1e-3 --gradient_clip_val=1 --gpus 1 --cnn rn34 --num_layers 4 --num_heads 8 --batch_size 4 --accumulate_grad_batches 8 --data_dir project/data/mp4 --model VTN_HCPF  --num_classes 10
+
+python train.py --log_dir project/log --dataset handcrop_poseflow --num_workers 4 --sequence_length 25 --temporal_stride 1 --learning_rate 1e-4 --gradient_clip_val=1 --gpus 1 --cnn rn34 --num_layers 4 --dropout 0.25 --num_heads 8 --batch_size 4 --accumulate_grad_batches 8 --data_dir project/data/mp4 --model VTN_HCPF  --num_classes 10
+
+python train.py --log_dir project/log --dataset handcrop_poseflow --num_workers 4 --sequence_length 25 --temporal_stride 1 --learning_rate 1e-3 --gradient_clip_val=1 --gpus 1 --cnn rn34 --num_layers 4 --dropout 0.25 --num_heads 8 --batch_size 4 --accumulate_grad_batches 8 --data_dir project/data/mp4 --model VTN_HCPF  --num_classes 10
+
+python train.py --log_dir project/log --dataset handcrop_poseflow --num_workers 4 --sequence_length 25 --temporal_stride 2 --learning_rate 1e-4 --gradient_clip_val=1 --gpus 1 --cnn rn34 --num_layers 4 --dropout 0.25 --num_heads 8 --batch_size 4 --accumulate_grad_batches 8 --data_dir project/data/mp4 --model VTN_HCPF  --num_classes 10
+
+python train.py --log_dir project/log --dataset handcrop_poseflow --num_workers 4 --sequence_length 25 --temporal_stride 2 --learning_rate 1e-3 --gradient_clip_val=1 --gpus 1 --cnn rn34 --num_layers 4 --dropout 0.25 --num_heads 8 --batch_size 4 --accumulate_grad_batches 8 --data_dir project/data/mp4 --model VTN_HCPF  --num_classes 10
 
