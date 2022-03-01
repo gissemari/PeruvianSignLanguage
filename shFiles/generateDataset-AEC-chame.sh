9f14ead0d8221ed2f050e16be1ed594650f94436
Chameleon Cloud User <cc@lrec-workshop.novalocal>
2022-03-01 04:13:07 +0000

    chameleon - generate dataset sh

--- shFiles/generateDataset-AEC-chame.sh
+++ shFiles/generateDataset-AEC-chame.sh
@@ -1,23 +1,10 @@
-source activate chalearn
-# copy utils folder into these directories
-PATH_ASL="/home/cc/ASL/PeruvianSignLanguage"
-cp -r $PATH_ASL/utils $PATH_ASL/2.Segmentation
-cp -r $PATH_ASL/utils $PATH_ASL/4.Translation/FrameToKeypoint
-cp -r $PATH_ASL/utils $PATH_ASL/5.Preparation
+cd ..
 
-#python 2.Segmentation/segmentPerSRT.py --rawVideoPath $PATH_ASL/Data/AEC/Videos/RawVideo/ --srtPath $PATH_ASL/Data/AEC/SRT/SRT_SIGN/ --outputVideoPath $PATH_ASL/Data/AEC/Videos/SEGMENTED_SIGN/ --flgGesture 1 --width 220 --height 220 --x1 380 --y1 988
+#python 1.Preprocessing/Video/cropInterpreterBySRT.py --rawVideoPath ./Data/AEC/Videos/RawVideo/ --srtPath ./Data/AEC/SRT/SRT_SIGN/ --outputVideoPath ./Data/AEC/Videos/SEGMENTED_SIGN/ --flgGesture 1 --x1 380 --y1 988 --width 220 --height 220
 
-python 4.Translation/FrameToKeypoint/ConvertVideoToDict.py --inputPath $PATH_ASL/Data/AEC/Videos/SEGMENTED_SIGN/ --dict_output $PATH_ASL/Data/AEC/Dataset/dict/ --keypoints_output $PATH_ASL/Data/AEC/Dataset/keypoints/
+python 1.Preprocessing/Video/generateKeypoints.py --inputPath ./Data/AEC/Videos/SEGMENTED_SIGN/  --dict_output ./Data/AEC/ --keypoints_output ./Data/AEC/Keypoints/pkl
 
-# ConvertVideoToDict was replaced by multiprocess
-#python 4.Translation/FrameToKeypoint/ConvertVideoToDict.py --inputPath $PATH_ASL/Data/AEC/Videos/SEGMENTED_SIGN/ --img_output  $PATH_ASL/Data/AEC/Dataset/img/  --dict_output $PATH_ASL/Data/AEC/Dataset/dict/ --keypoints_output $PATH_ASL/Data/AEC/Dataset/keypoints/
-
-#python 5.Preparation/DictToSample.py --dict_Path ./Data/Dataset/dict/dict.json --shuffle --leastValue --output_Path ./Data/Dataset/readyToRun/ --words 10
-
-# errase created files to avoid confusions
-rm -r /home/bejaranog/signLanguage/PeruvianSignLanguage/2.Segmentation/utils
-rm -r /home/bejaranog/signLanguage/PeruvianSignLanguage/4.Translation/FrameToKeypoint/utils
-#rm -r /home/bejaranog/signLanguage/PeruvianSignLanguage/5.Preparation/utils
+python 1.Preprocessing/Selector/DictToSample.py --dict_Path ./Data/AEC/dict.json --shuffle --leastValue --output_Path ./Data/AEC/Selected/ --words 10 --words_File ./Data/list10.csv
 
 echo "Press ENTER to exit:"
 read ENTER
