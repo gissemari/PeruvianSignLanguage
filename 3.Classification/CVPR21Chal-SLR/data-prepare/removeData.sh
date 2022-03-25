49d85fe5c24292383209e4d74b27aab42fb29049
Chameleon Cloud User <cc@lsp-models.novalocal>
2022-03-25 17:59:13 +0000

    to errase the data in CVPR

---
+++ 3.Classification/CVPR21Chal-SLR/data-prepare/removeData.sh
@@ -0,0 +1,11 @@
+rm -v data/sign/train/*
+rm -v data/sign/test/*
+rm -v data/sign/val/*
+
+rm -v data/npy3/train/*
+rm -v data/npy3/test/*
+rm -v data/npy3/val/*
+
+rm -v data/features/train/*
+rm -v data/features/test/*
+rm -v data/features/val/*
