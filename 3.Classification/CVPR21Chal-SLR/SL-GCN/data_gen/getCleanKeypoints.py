f7db6e046db7b141f0c1173cb2529a34371b1a00
JoeNatan30 <huamani.jn@pucp.edu.pe>
2022-05-20 17:27:57 -0500

    smilelab- SL - data gen py

--- 3.Classification/CVPR21Chal-SLR/SL-GCN/data_gen/getCleanKeypoints.py
+++ 3.Classification/CVPR21Chal-SLR/SL-GCN/data_gen/getCleanKeypoints.py
@@ -8,6 +8,8 @@ import pandas as pd
 import time
 from collections import Counter
 
+import cv2
+
 sys.path.extend(['../'])
 
 selected_joints = {
@@ -15,13 +17,12 @@ selected_joints = {
     '31': np.concatenate((np.arange(0,11), [91,95,96,99,100,103,104,107,108,111],[112,116,117,120,121,124,125,128,129,132]), axis=0), #31
     '27': np.concatenate(([0,5,6,7,8,9,10],
                     [91,95,96,99,100,103,104,107,108,111],[112,116,117,120,121,124,125,128,129,132]), axis=0), #27
-    '1': np.arange(0,133) #np.concatenate((np.arange(0,17) ,np.arange(17,20),np.arange(20,23)),axis=0)
+    '1': np.arange(0,27) #np.concatenate((np.arange(0,17) ,np.arange(17,20),np.arange(20,23)),axis=0)
 }
 
 max_body_true = 1
 max_frame = 150
 num_channels = 2#3
-
 
 def gendata(data_path, out_path, part='train', config='27'):
 
@@ -41,20 +42,25 @@ def gendata(data_path, out_path, part='train', config='27'):
     meaning = {lab:idx for idx,lab in zip(ind,labelIndex)}
 
     listInstance = []
-
+    print(len(df[data]),len(df[data][0]),len(df[data][0][0]),len(df[data][0][0][0]))
     fp = np.zeros((len(df[names]), max_frame, num_joints, num_channels, max_body_true), dtype=np.float32)
     sample_names = []
     for pos in range(len(df[names])):
 
         xCoords = df[data][pos][0]
         yCoords = df[data][pos][1]
-        # [0.5 for _ in range(27)]
-        skel = np.asarray([list(zip(x*256,y*256)) for (x, y) in zip(xCoords,yCoords)])
+
+        #print(yCoords)
+
+        # [0.5 for _ in range(27)]  
+        skel = np.asarray([list(zip(x,y)) for (x, y) in zip(xCoords,yCoords)])
+
         name = df[names][pos]
         sample_names.append(name)
         label = meaning[df[labels_key][pos]]
         #print(meaning[df[labels_key][pos]])
         labels.append(int(label))
+        
 
         if skel.shape[0] < max_frame:
             L = skel.shape[0] 
@@ -87,7 +93,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Sign Data Converter.')
     parser.add_argument('--data_path', default='./../../../../Data/AEC_cleaned/data_10_10_24.pk') #'train_npy/npy', 'va_npy/npy'
     parser.add_argument('--out_folder', default='../data/sign/')
-    parser.add_argument('--points', default='27') #'27')
+    parser.add_argument('--points', default='1') #'27')
 
     arg = parser.parse_args()
 
@@ -98,13 +104,15 @@ if __name__ == '__main__':
         os.makedirs(out_path)
 
     gendata(
-        './../../../../Data/AEC_cleaned/data_10_10_27-train.pk',
+        #'./../../../../Data/AEC_cleaned/data_10_10_27-train.pk',
+        './../../../../Data/merged/AEC-PUCP_PSL_DGI156/merge-train.pk',
         out_path,
         part="train",
         config=arg.points)
 
     gendata(
-        './../../../../Data/AEC_cleaned/data_10_10_27-val.pk',
+        #'./../../../../Data/AEC_cleaned/data_10_10_27-val.pk',
+        './../../../../Data/merged/AEC-PUCP_PSL_DGI156/merge-val.pk',
         out_path,
         part="val",
         config=arg.points)
--- 3.Classification/CVPR21Chal-SLR/SL-GCN/data_gen/getKeypointsRawTrainVal.py
+++ 3.Classification/CVPR21Chal-SLR/SL-GCN/data_gen/getKeypointsRawTrainVal.py
@@ -3,6 +3,7 @@ import pickle
 from tqdm import tqdm
 import sys
 import numpy as np
+import pandas as pd
 import os
 
 sys.path.extend(['../'])
@@ -16,7 +17,13 @@ selected_joints = {
 
 max_body_true = 1
 max_frame = 150
-num_channels = 3
+num_channels = 2
+
+char_to_replace = {'Á': 'A',
+                   'É': 'E',
+                   'Í': 'I',
+                   'Ó': 'O',
+                   'Ú': 'U'}
 
 def gendata(data_path, label_path, out_path, part='train', config='27'):
     labels = []
@@ -24,29 +31,39 @@ def gendata(data_path, label_path, out_path, part='train', config='27'):
     sample_names = []
     selected = selected_joints[config]
     num_joints = len(selected)
-    label_file = open(label_path, 'r', encoding='utf-8')
 
-    for line in label_file.readlines():
-        line = line.strip()
-        line = line.split(',')
-        #print(line[0])
-        sample_names.append(line[0])
-        data.append(os.path.join(data_path, line[0] + '_color.mp4.npy'))
-        #print(line[1])
-        labels.append(int(line[1]))
-        # print(labels[-1])
+    meaning_temp =  pd.read_json('/'.join(label_path.split('/')[:-1]) + '/meaning.json')
+    meaning = dict()
+    for key, value in meaning_temp[0].items():
+        word = key.upper()
+        for _key, _value in char_to_replace.items():
+            word = word.replace(_key, _value)
+        meaning[word]= value
+
+    label_file = pd.read_json(label_path)
+
+    for line in list(label_file[0]):
+        
+        sample = line.upper()
+        for key, value in char_to_replace.items():
+            sample = sample.replace(key, value)
+
+        word = sample.split('_')[0]
+
+        sample_names.append(sample)
+
+        data.append(os.path.join(data_path, line + '.mp4.npy'))
+        labels.append(meaning[word])
 
     fp = np.zeros((len(data), max_frame, num_joints, num_channels, max_body_true), dtype=np.float32)
 
     for i, data_path in enumerate(data):
-
         # print(sample_names[i])
         skel = np.load(data_path)
-        skel = skel[:,selected,:]
-        print(skel)
+        skel = skel[:,selected,:2]
         if skel.shape[0] < max_frame:
             L = skel.shape[0]
-            print(L)
+
             fp[i,:L,:,:,0] = skel
 
             rest = max_frame - L
@@ -56,10 +73,10 @@ def gendata(data_path, label_path, out_path, part='train', config='27'):
 
         else:
             L = skel.shape[0]
-            print(L)
+
             fp[i,:,:,:,0] = skel[:max_frame,:,:]
 
-    print(sample_names,labels)
+    #print(sample_names,labels)
     with open('{}/{}_label.pkl'.format(out_path, part), 'wb') as f:
         pickle.dump((sample_names, labels), f)
 
@@ -68,17 +85,13 @@ def gendata(data_path, label_path, out_path, part='train', config='27'):
     np.save('{}/{}_data_joint.npy'.format(out_path, part), fp)
 
 
-
-
-
-
 if __name__ == '__main__':
 
-    part = 'train' #'test' # 'train', 'val'
+    part = 'val' #'test' # 'train', 'val'
 
     parser = argparse.ArgumentParser(description='Sign Data Converter.')
     parser.add_argument('--data_path', default=f'../../data-prepare/data/npy3/{part}') #'train_npy/npy', 'va_npy/npy'
-    parser.add_argument('--label_path', default=f'../../..//ChaLearn-2021-LAP/data/{part}_labels.csv') #'../data/sign/27/train_labels.csv') # 'train_labels.csv', 'val_gt.csv', 'test_labels.csv'
+    parser.add_argument('--label_path', default=f"../../../../Data/merged/AEC-PUCP_PSL_DGI156/names-{part}.json") #'../data/sign/27/train_labels.csv') # 'train_labels.csv', 'val_gt.csv', 'test_labels.csv'
     parser.add_argument('--out_folder', default='../data/sign/')
     parser.add_argument('--points', default='27')
 
