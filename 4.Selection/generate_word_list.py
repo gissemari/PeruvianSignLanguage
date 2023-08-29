4afa2214d5d164db35ecbdc4c8d9b9f5ca0c89fe
Joe Huamani <huamani.jn@pucp.edu.pe>
2023-08-29 00:01:51 +0000

    selection scripts

---
+++ 4.Selection/generate_word_list.py
@@ -0,0 +1,39 @@
+# Standard library imports
+import argparse
+import os
+import sys
+import random
+
+# Third party imports
+import pandas as pd
+pd.set_option("display.max_colwidth", 15) 
+
+# Local imports
+sys.path.append('../')
+import utils.video as uv
+
+parser = argparse.ArgumentParser(description='generate a csv list of words in different orders')
+parser.add_argument('--dict_input', type=str,  required=True, help='relative path of scv output set of landmarks.')
+parser.add_argument('--seed', type=int, default=42)
+#parser.add_argument('--csv_name', type=str, required=True)
+args = parser.parse_args()
+
+random_seed = args.seed
+random.seed(random_seed)
+
+dict_input = os.path.normpath(args.dict_input)
+df = uv.get_list_from_json_dataset(dict_input)
+
+unique_labels = df['label'].unique()
+
+random.shuffle(unique_labels)
+
+randomized_df = pd.DataFrame({'label': unique_labels})
+
+csv_name = os.path.splitext(os.path.basename(dict_input))[0] + f'_V{args.seed}.csv' #args.csv_name
+csv_dirname = os.path.dirname(dict_input)
+csv_output_path = os.sep.join([csv_dirname, csv_name])
+
+randomized_df.to_csv(csv_output_path, index=False, header=False)
+
+print(randomized_df)
---
+++ 4.Selection/splitter.py
@@ -0,0 +1,70 @@
+# Standard library imports
+import argparse
+import os
+import sys
+
+# Third party imports
+import pandas as pd
+pd.set_option("display.max_colwidth", 15) 
+
+# Local imports
+sys.path.append('../')
+import utils.video as uv
+
+# Title
+parser = argparse.ArgumentParser(description='To split the dataset metadata to use it posteriorly to feed an IA')
+
+parser.add_argument('--dict_output', type=str,  required=True, help='relative path of scv output set of landmarks.')
+parser.add_argument('--json_name', type=str,  required=True,)
+
+parser.add_argument('--top', type=int, default=-1)
+parser.add_argument('--min_instances', type=int, default=-1)
+parser.add_argument('--max_instances', type=int, default=-1)
+parser.add_argument('--banned_words', action='store_true',)
+parser.add_argument('--selected_words', action='store_true',)
+
+args = parser.parse_args()
+
+
+def limit_instances(group, max_instances_numb):
+    if len(group) > max_instances_numb:
+        return group.head(max_instances_numb)
+    else:
+        return group
+
+
+dict_output = os.path.normpath(os.sep.join([args.dict_output,"dict.json"]))
+
+filtered_df = uv.get_list_from_json_dataset(dict_output)
+
+# Filter: Get the top # most frecuent labels
+if args.top >0:
+    label_counts = filtered_df['label'].value_counts()
+    top_labels = label_counts.head(args.top).index.tolist()
+    filtered_df = filtered_df[filtered_df['label'].isin(top_labels)]
+
+# Filter: Keep labels with more than # instances
+if args.min_instances >= 0:
+    filtered_df = filtered_df.groupby('label').filter(lambda x: len(x) > args.min_instances)
+
+# Filter: Limit to a maximum of # instances per label
+if args.max_instances > 3:
+    filtered_df = filtered_df.groupby('label').apply(limit_instances, args.max_instances)
+
+# Filter: List of labels to keep
+if args.selected_words:
+    labels_to_keep = pd.read_csv('selected_words.csv', header=None)
+    filtered_df = filtered_df[filtered_df['label'].isin(labels_to_keep[0])]
+
+# Filter:List of labels to remove
+if args.banned_words:
+    labels_to_remove = pd.read_csv('banned_words.csv', header=None)
+    filtered_df = filtered_df[~filtered_df['label'].isin(labels_to_remove[0])]
+
+# To reset the DataFrame to its initial form
+filtered_df = filtered_df.reset_index(drop=True)
+
+print(filtered_df)
+print("Remember: The gloss_id value is changed when the dict.json is reconstructed")
+
+uv.reconstruct_json(filtered_df, os.sep.join([args.dict_output, args.json_name]))
